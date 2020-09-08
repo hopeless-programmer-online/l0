49cf700328f69425d83f2600df249ef889efab9e
hopeless-programmer-online <63446812+hopeless-programmer-online@users.noreply.github.com>
2020-09-08 11:46:06 +0300

    Export external instruction

--- src/back.ts
+++ src/back.ts
@@ -1,7 +1,9 @@
 import Machine from "./back/machine";
 import Instruction from "./back/instruction";
+import ExternalInstruction from "./back/external-instruction";
 
 export {
     Machine,
     Instruction,
+    ExternalInstruction,
 };
