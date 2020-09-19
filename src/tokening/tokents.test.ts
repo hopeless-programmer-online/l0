61fa843a78079411388ef2fb93a6612feb4552e5
hopeless-programmer-online <63446812+hopeless-programmer-online@users.noreply.github.com>
2020-09-19 23:49:44 +0300

    Update tokents.test.ts

--- src/tokening/tokents.test.ts
+++ src/tokening/tokents.test.ts
@@ -9,6 +9,7 @@ import PlainWord from "./plain-word";
 it(``, () => {
     const tokens = [ ...Tokens.FromString(`u, v : f(x, y)`) ];
 
+    // u
     expect(tokens[0]).toBeInstanceOf(NameToken);
 
     const u = tokens[0] as NameToken;
@@ -20,25 +21,66 @@ it(``, () => {
 
     expect(u0.Text).toBe(`u`);
 
-    // expect(tokens[1]).toBeInstanceOf(CommaToken);
+    // ,
+    expect(tokens[1]).toBeInstanceOf(CommaToken);
 
-    // expect(tokens[2]).toBeInstanceOf(NameToken);
-    // expect((tokens[2] as NameToken).String).toBe(`v`);
+    // v
+    expect(tokens[2]).toBeInstanceOf(NameToken);
 
-    // expect(tokens[3]).toBeInstanceOf(ColonToken);
+    const v = tokens[2] as NameToken;
 
-    // expect(tokens[4]).toBeInstanceOf(NameToken);
-    // expect((tokens[4] as NameToken).String).toBe(`f`);
+    expect(v.Words).toHaveLength(1);
+    expect(v.Words[0]).toBeInstanceOf(PlainWord);
 
-    // expect(tokens[5]).toBeInstanceOf(OpeningRoundBraceToken);
+    const v0 = v.Words[0] as PlainWord;
 
-    // expect(tokens[6]).toBeInstanceOf(NameToken);
-    // expect((tokens[6] as NameToken).String).toBe(`x`);
+    expect(v0.Text).toBe(`v`);
 
-    // expect(tokens[7]).toBeInstanceOf(CommaToken);
+    // :
+    expect(tokens[3]).toBeInstanceOf(ColonToken);
 
-    // expect(tokens[8]).toBeInstanceOf(NameToken);
-    // expect((tokens[8] as NameToken).String).toBe(`y`);
+    // f
+    expect(tokens[4]).toBeInstanceOf(NameToken);
 
-    // expect(tokens[9]).toBeInstanceOf(ClosingRoundBraceToken);
+    const f = tokens[4] as NameToken;
+
+    expect(f.Words).toHaveLength(1);
+    expect(f.Words[0]).toBeInstanceOf(PlainWord);
+
+    const f0 = f.Words[0] as PlainWord;
+
+    expect(f0.Text).toBe(`f`);
+
+    // (
+    expect(tokens[5]).toBeInstanceOf(OpeningRoundBraceToken);
+
+    // x
+    expect(tokens[6]).toBeInstanceOf(NameToken);
+
+    const x = tokens[6] as NameToken;
+
+    expect(x.Words).toHaveLength(1);
+    expect(x.Words[0]).toBeInstanceOf(PlainWord);
+
+    const x0 = x.Words[0] as PlainWord;
+
+    expect(x0.Text).toBe(`x`);
+
+    // ,
+    expect(tokens[7]).toBeInstanceOf(CommaToken);
+
+    // y
+    expect(tokens[8]).toBeInstanceOf(NameToken);
+
+    const y = tokens[8] as NameToken;
+
+    expect(y.Words).toHaveLength(1);
+    expect(y.Words[0]).toBeInstanceOf(PlainWord);
+
+    const y0 = y.Words[0] as PlainWord;
+
+    expect(y0.Text).toBe(`y`);
+
+    // )
+    expect(tokens[9]).toBeInstanceOf(ClosingRoundBraceToken);
 });
