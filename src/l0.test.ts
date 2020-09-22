db4e8471b80fed311faded3524a4294a4a3432a9
hopeless-programmer-online <63446812+hopeless-programmer-online@users.noreply.github.com>
2020-09-22 22:15:37 +0300

    Fix missing back

--- src/l0.test.ts
+++ src/l0.test.ts
@@ -4,3 +4,23 @@ it(`Should be an object`, () => {
     expect(typeof l0).toBe(`object`);
     expect(l0).not.toBe(null);
 });
+
+it(`Should be an object`, () => {
+    expect(l0).toHaveProperty(`front`);
+    expect(l0.front).not.toBe(null);
+});
+
+it(`Should be an object`, () => {
+    expect(l0).toHaveProperty(`back`);
+    expect(l0.back).not.toBe(null);
+});
+
+it(`Should be an object`, () => {
+    expect(l0).toHaveProperty(`tokening`);
+    expect(l0.tokening).not.toBe(null);
+});
+
+it(`Should be an object`, () => {
+    expect(l0).toHaveProperty(`parsing`);
+    expect(l0.parsing).not.toBe(null);
+});
