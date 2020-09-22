c81b98f6281f4e2b9d13a7967c01db996800e4b3
hopeless-programmer-online <63446812+hopeless-programmer-online@users.noreply.github.com>
2020-09-23 01:16:44 +0300

    Expose tokens

--- src/tokening.ts
+++ src/tokening.ts
@@ -1,5 +1,10 @@
 import Token from "./tokening/token";
 import Tokens from "./tokening/tokens";
+import Word from "./tokening/word";
+import PlainWord from "./tokening/plain-word";
+import QuotedWord from "./tokening/quoted-word";
+import Name from "./tokening/name";
+import NameToken from "./tokening/name-token";
 import CommaToken from "./tokening/comma-token";
 import ColonToken from "./tokening/colon-token";
 import OpeningRoundBraceToken from "./tokening/opening-round-brace-token";
@@ -10,6 +15,11 @@ import ClosingFigureBraceToken from "./tokening/closing-figure-brace-token";
 export {
     Token,
     Tokens,
+    Word,
+    PlainWord,
+    QuotedWord,
+    Name,
+    NameToken,
     CommaToken,
     ColonToken,
     OpeningRoundBraceToken,
