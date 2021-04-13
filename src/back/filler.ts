b7b1b9462d7fc4dbf32f7f824656ba023f78a977
hopeless programmer <me@hopeless-programmer.online>
2021-04-13 21:55:49 +0300

    Update filler.ts

--- src/back/filler.ts
+++ src/back/filler.ts
@@ -1,6 +1,5 @@
+import { Instruction, InternalInstruction, ExternalInstruction, TerminalInstruction, BindInstruction, Template } from '../back'
 import { ExplicitParameter as Parameter } from '../front'
-import BindInstruction from './bind-instruction'
-import ExternalInstruction from './external-instruction'
 
 export default class Filler {
     private bind : BindInstruction
@@ -26,16 +25,108 @@ export default class Filler {
         }
 
         switch (text) {
-            case    'nothing' : return undefined
-            case    'true'    : return true
-            case    'false'   : return false
-            case    'bind'    : return this.bind
-            case    'print'   : return new ExternalInstruction({ callback : buffer => {
-                console.log(...buffer.slice(2))
+            case 'nothing' : return undefined
+            case 'true' : return true
+            case 'false' : return false
+            case 'bind' : return this.bind
+            case 'print' : return new ExternalInstruction({ callback : ([ _, next, ...values ]) => {
+                console.log(...values)
 
-                return [ buffer[1], buffer[1] ]
+                return [ next, next ]
             } })
-            default         : throw new Error(`Can't fill parameter ${text}`)
+            case 'is instruction' : return new ExternalInstruction({ callback : ([ _, next, target ]) => {
+                const result = target instanceof Instruction
+
+                return [ next, next, result ]
+            } })
+            case 'is internal instruction' : return new ExternalInstruction({ callback : ([ _, next, target ]) => {
+                const result = target instanceof InternalInstruction
+
+                return [ next, next, result ]
+            } })
+            case 'is external instruction' : return new ExternalInstruction({ callback : ([ _, next, target ]) => {
+                const result = target instanceof ExternalInstruction
+
+                return [ next, next, result ]
+            } })
+            case 'is terminal instruction' : return new ExternalInstruction({ callback : ([ _, next, target ]) => {
+                const result = target instanceof TerminalInstruction
+
+                return [ next, next, result ]
+            } })
+            case 'get template' : return new ExternalInstruction({ callback : ([ _, next, instruction ]) => {
+                if (!(instruction instanceof InternalInstruction)) throw new Error
+
+                return [ next, next, instruction.template ]
+            } })
+            case 'is template' : return new ExternalInstruction({ callback : ([ _, next, target ]) => {
+                const result = target instanceof Template
+
+                return [ next, next, result ]
+            } })
+            case 'get buffer' : return new ExternalInstruction({ callback : ([ _, next, instruction ]) => {
+                if (!(instruction instanceof InternalInstruction)) throw new Error
+
+                return [ next, next, instruction.buffer.slice() ]
+            } })
+            case 'get targets' : return new ExternalInstruction({ callback : ([ _, next, template ]) => {
+                if (!(template instanceof Template)) throw new Error
+
+                return [ next, next, template.targets ]
+            } })
+            case 'if' : return new ExternalInstruction({ callback : ([ _, next, condition, then ]) => {
+                if (condition) return [ then, next ]
+
+                return [ next, next ]
+            } })
+            case 'equal' : return new ExternalInstruction({ callback : ([ _, next, a, b ]) => {
+                return [ next, next, a === b ]
+            } })
+            case 'at' : return new ExternalInstruction({ callback : ([ _, next, a, i ]) => {
+                return [ next, next, a[i] ]
+            } })
+            case 'is_array' : return new ExternalInstruction({ callback : ([ _, next, a ]) => {
+                return [ next, next, Array.isArray(a) ]
+            } })
+            case 'array' : return new ExternalInstruction({ callback : ([ _, next, ...content ]) => {
+                return [ next, next, content ]
+            } })
+            case 'length' : return new ExternalInstruction({ callback : ([ _, next, a ]) => {
+                return [ next, next, a.length ]
+            } })
+            case 'concat' : return new ExternalInstruction({ callback : ([ _, next, a, b ]) => {
+                return [ next, next, [ ...a, ...b ] ]
+            } })
+            case 'slice' : return new ExternalInstruction({ callback : ([ _, next, a, b, e ]) => {
+                return [ next, next, a.slice(b, e) ]
+            } })
+            case 'push_back' : return new ExternalInstruction({ callback : ([ _, next, a, x ]) => {
+                a.push(x)
+
+                return [ next, next ]
+            } })
+            case 'push_front' : return new ExternalInstruction({ callback : ([ _, next, a, x ]) => {
+                a.unshift(x)
+
+                return [ next, next ]
+            } })
+            case 'minus' : return new ExternalInstruction({ callback : ([ _, next, a, b ]) => {
+                return [ next, next, a - b ]
+            } })
+            case 'plus' : return new ExternalInstruction({ callback : ([ _, next, a, b ]) => {
+                return [ next, next, a + b ]
+            } })
+            case 'multiply' : return new ExternalInstruction({ callback : ([ _, next, a, b ]) => {
+                return [ next, next, a * b ]
+            } })
+            case 'not' : return new ExternalInstruction({ callback : ([ _, next, a ]) => {
+                return [ next, next, !a ]
+            } })
+            case 'less' : return new ExternalInstruction({ callback : ([ _, next, a, b ]) => {
+                return [ next, next, a < b ]
+            } })
+
+            default : throw new Error(`Can't fill parameter ${text}`)
         }
     }
 }
