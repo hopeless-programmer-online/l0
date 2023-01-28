889909efbbac659054d115706a55fca89082ac26
Hopeless Programmer <me@hopeless-programmer.online>
2023-01-28 03:26:03 +0200

    Add syntax analyzer

---
+++ src/syntax.test.ts
@@ -0,0 +1,46 @@
+import { Analyzer as Lexer } from './lexis'
+import {
+    Main,
+    Program,
+    Parameters,
+    Parameter,
+    Commands,
+    Declaration,
+    Call,
+    Inputs,
+    Input,
+    Outputs,
+    Output,
+    Analyzer,
+} from './syntax'
+
+function parse(text : string) {
+    const lexer = new Lexer
+    const lexemes = lexer.analyze(text)
+    const parser = new Analyzer
+    const main = parser.analyze(lexemes)
+
+    return main
+}
+
+test(`Check export`, () => {
+    expect(Main).toBeDefined()
+    expect(Program).toBeDefined()
+    expect(Parameters).toBeDefined()
+    expect(Parameter).toBeDefined()
+    expect(Commands).toBeDefined()
+    expect(Declaration).toBeDefined()
+    expect(Call).toBeDefined()
+    expect(Inputs).toBeDefined()
+    expect(Input).toBeDefined()
+    expect(Outputs).toBeDefined()
+    expect(Output).toBeDefined()
+})
+
+describe(`Check analyzer`, () => {
+    test(`Smoke test`, () => {
+        expect(parse(`f(){}`)).toMatchObject({
+            symbol : Main.symbol,
+        })
+    })
+})
--- src/syntax.ts
+++ src/syntax.ts
@@ -1,38 +1,68 @@
-export abstract class Program {
-    public readonly parameters : Parameters
-    public readonly commands : Commands
+import * as lexis from './lexis'
 
-    public constructor() {
-        throw new Error // @todo
+export type Text = string
+
+export abstract class Word {
+    public readonly text : Text
+
+    public constructor({ text } : { text : Text }) {
+        this.text = text
     }
 }
 
-export class MainProgram extends Program {
-    public static readonly symbol : unique symbol = Symbol(`l0.syntax.MainProgram`)
+export class BareWord extends Word {
+    public static readonly symbol : unique symbol = Symbol(`l0.syntax.BareWord`)
 
-    public readonly symbol : typeof MainProgram.symbol = MainProgram.symbol
+    public readonly symbol : typeof BareWord.symbol = BareWord.symbol
 }
 
-export class DeclaredProgram extends Program {
-    public static readonly symbol : unique symbol = Symbol(`l0.syntax.DeclaredProgram`)
+export class QuotedWord extends Word {
+    public static readonly symbol : unique symbol = Symbol(`l0.syntax.QuotedWord`)
 
-    public readonly symbol : typeof DeclaredProgram.symbol = DeclaredProgram.symbol
-    public readonly declaration : DeclarationCommand
+    public readonly symbol : typeof QuotedWord.symbol = QuotedWord.symbol
+}
 
-    public constructor() {
-        throw new Error // @todo
+export type WordUnion = BareWord | QuotedWord
 
-        super()
+export class Name {
+    public readonly words : WordUnion[]
+
+    public constructor({ words } : { words : WordUnion[] }) {
+        this.words = words
     }
+
+    public get overlapped() {
+        const words = this.words.slice()
+        const first = words[0]
+
+        if (first.symbol === BareWord.symbol) words[0] = new BareWord({ text : `/` + first.text })
+        else words.unshift(new BareWord({ text : `/` }))
+
+        return new Name({ words })
+    }
+}
+
+export class Scope {
+}
+
+export abstract class GenericProgram {
+    public readonly parameters = new Parameters
+    public readonly commands = new Commands
+}
+
+export class Main extends GenericProgram {
+    public static readonly symbol : unique symbol = Symbol(`l0.syntax.Main`)
+
+    public readonly symbol : typeof Main.symbol = Main.symbol
+}
+
+export class Program extends GenericProgram {
+    public static readonly symbol : unique symbol = Symbol(`l0.syntax.Program`)
+
+    public readonly symbol : typeof Program.symbol = Program.symbol
 }
 
 export class Parameters {
-    public readonly program : Program
-
-    public constructor() {
-        throw new Error // @todo
-    }
-
     public * [Symbol.iterator]() : Iterator<ParameterUnion, void> {
     }
 }
@@ -61,29 +91,18 @@ export class ExplicitParameter extends Parameter {
 export type ParameterUnion = ClosureParameter | SuperParameter | ExplicitParameter
 
 export class Commands {
-    public readonly program : Program
-
-    public constructor() {
-        throw new Error // @todo
-    }
-
     public * [Symbol.iterator]() : Iterator<CommandUnion, void> {
     }
 }
 
 export abstract class Command {
-    public readonly commands : Commands
-
-    public constructor() {
-        throw new Error // @todo
-    }
 }
 
-export class DeclarationCommand extends Command {
+export class Declaration extends Command {
     public static readonly symbol : unique symbol = Symbol(`l0.syntax.DeclarationCommand`)
 
-    public readonly symbol : typeof DeclarationCommand.symbol = DeclarationCommand.symbol
-    public readonly program : DeclaredProgram
+    public readonly symbol : typeof Declaration.symbol = Declaration.symbol
+    public readonly program : Program
 
     public constructor() {
         throw new Error // @todo
@@ -92,10 +111,11 @@ export class DeclarationCommand extends Command {
     }
 }
 
-export class CallCommand extends Command {
+export class Call extends Command {
     public static readonly symbol : unique symbol = Symbol(`l0.syntax.CallCommand`)
 
-    public readonly symbol : typeof CallCommand.symbol = CallCommand.symbol
+    public readonly symbol : typeof Call.symbol = Call.symbol
+    // public readonly target
     public readonly inputs : Inputs
     public readonly outputs : Outputs
 
@@ -106,44 +126,22 @@ export class CallCommand extends Command {
     }
 }
 
-export type CommandUnion = DeclarationCommand | CallCommand
+export type CommandUnion = Declaration | Call
 
 export class Inputs {
-    public readonly call : CallCommand
-
-    public constructor() {
-        throw new Error // @todo
-    }
-
     public * [Symbol.iterator]() : Iterator<Input, void> {
     }
 }
 
 export class Input {
-    public readonly inputs : Inputs
-
-    public constructor() {
-        throw new Error // @todo
-    }
 }
 
 export class Outputs {
-    public readonly call : CallCommand
-
-    public constructor() {
-        throw new Error // @todo
-    }
-
     public * [Symbol.iterator]() : Iterator<OutputUnion, void> {
     }
 }
 
 export abstract class Output {
-    public readonly outputs : Outputs
-
-    public constructor() {
-        throw new Error // @todo
-    }
 }
 
 export class SubOutput extends Output {
@@ -159,3 +157,86 @@ export class ExplicitOutput extends Output {
 }
 
 export type OutputUnion = SubOutput | ExplicitOutput
+
+class Walker {
+    private readonly lexemes : lexis.Children
+    private index = 0
+
+    public readonly program : GenericProgram
+
+    public constructor({
+        lexemes,
+        program,
+    } : {
+        lexemes : lexis.Children
+        program : GenericProgram
+    }) {
+        this.lexemes = lexemes
+        this.program = program
+    }
+
+    public get current() {
+        const { lexemes, index } = this
+
+        if (index >= lexemes.length) return null
+
+        return lexemes[index]
+    }
+    public get next() {
+        const { lexemes } = this
+
+        if (this.index >= lexemes.length) return null
+
+        ++this.index
+
+        if (this.index >= lexemes.length) return null
+
+        return lexemes[this.index]
+    }
+}
+
+export class Analyzer {
+    public analyze(lexemes : lexis.Children) {
+        const program = new Main
+        let walker = new Walker({ lexemes, program })
+        const nesting : Walker[] = [ walker ]
+
+        while (true) {
+            const first = walker.current
+
+            if (!first) {
+                nesting.pop()
+
+                if (nesting.length < 1) break
+            }
+            else if (first.symbol === lexis.Name.symbol) {
+                const second = walker.next
+
+                if (!second) throw new Error(`Unexpected end of input.`)
+                if (second.symbol === lexis.Block.symbol) {
+                    if (second.opening.type !== lexis.BraceType.Round) throw new Error(`Unexpected ${second.opening.type} block.`)
+
+                    const third = walker.next
+
+                    if (!third) throw new Error(`Unexpected end of input.`)
+                    if (third.symbol === lexis.Block.symbol) {
+                        if (third.opening.type !== lexis.BraceType.Figure) throw new Error(`Unexpected ${third.opening.type} block.`)
+
+                        // @todo: add declaration
+
+                        walker = new Walker({
+                            lexemes : third.children,
+                            program : new Program,
+                        })
+                        nesting.push(walker)
+                    }
+                    else throw new Error(`Unexpected lexeme ${third}.`)
+                }
+                else throw new Error(`Unexpected lexeme ${second}.`)
+            }
+            else throw new Error(`Unexpected lexeme ${first}.`)
+        }
+
+        return program
+    }
+}
